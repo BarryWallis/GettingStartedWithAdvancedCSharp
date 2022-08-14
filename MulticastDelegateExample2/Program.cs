96687842bc768a9c3f11037c60604d15d9083829
Barry Wallis <barry.wallis@outlook.com>
2022-08-14 13:07:23 -0700

    Added MulticastDelegateExample2

---
+++ MulticastDelegateExample2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static int MethodOne()
+{
+    Console.WriteLine("A static method of Program - MethodOne() executed.");
+    return 1;
+}
+
+static int MethodTwo()
+{
+    Console.WriteLine("A static method of Program - MethodTwo() executed.");
+    return 2;
+}
+
+static int MethodThree()
+{
+    Console.WriteLine("A static method of Program - MethodThree() executed.");
+    return 3;
+}
+
+Console.WriteLine("*** A case study with a multicast delegate when we target non-void methods. ***");
+Console.WriteLine();
+
+MultiDelegate? _multiDelegate = MethodOne;
+_multiDelegate += MethodTwo;
+_multiDelegate += MethodThree;
+int finalValue = _multiDelegate();
+Console.WriteLine($"The final value is {finalValue}");
+
+internal delegate int MultiDelegate();
