61bbfc43beb656ccbbe3cf9e0f049307b023653f
Barry Wallis <barry.wallis@outlook.com>
2022-08-14 16:33:06 -0700

    Added EventsEx2

---
+++ EventsEx2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EventsEx2;
+
+Console.WriteLine("*** Exploring a Custom Event ***");
+
+Sender sender = new();
+Receiver receiver = new();
+sender.MyIntChanged += receiver.GetNotificationFromSender;
+
+sender.MyInt = 1;
+sender.MyInt = 2;
+sender.MyIntChanged -= receiver.GetNotificationFromSender;
+
+sender.MyInt = 3;
---
+++ EventsEx2/Receiver.cs
@@ -0,0 +1,7 @@
+﻿namespace EventsEx2;
+
+internal class Receiver
+{
+    public void GetNotificationFromSender(object sender, EventArgs eventArgs)
+        => Console.WriteLine("Receiver received a notification: Sender has changed the MyInt value.");
+}
