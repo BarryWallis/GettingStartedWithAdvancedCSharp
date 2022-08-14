a94d79d3a1e8283eefb875fb75e5bc4a073cc39c
Barry Wallis <barry.wallis@outlook.com>
2022-08-14 14:04:36 -0700

    Added CovarianceWithNonGenericDelegate.

---
+++ CovarianceWithNonGenericDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("*** Testing Covariance with Delegates ***");
+        VehicleDelegate vehicleDelegate1 = Vehicle.CreateVehicle;
+        _ = vehicleDelegate1();
+
+        VehicleDelegate vehicleDelegate2 = Bus.CreateBus;
+        _ = vehicleDelegate2();
+    }
+
+    public class Vehicle
+    {
+        public static Vehicle CreateVehicle()
+        {
+            Vehicle vehicle = new();
+            Console.WriteLine("Inside Vehicle.CreateVehicle() a vehicle is created.");
+            return vehicle;
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public static Bus CreateBus()
+        {
+            Bus bus = new();
+            Console.WriteLine("Inside Bus.CreateBus() a bus is created.");
+            return bus;
+        }
+    }
+
+
+    public delegate Vehicle VehicleDelegate();
+}
+
+
