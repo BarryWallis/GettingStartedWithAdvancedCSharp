d4691c479d179341cdd11fbb88f93b560edebe88
Barry Wallis <barry.wallis@outlook.com>
2022-08-15 15:59:30 -0700

    Added ExpressionBodiedMethodDemo

---
+++ ExpressionBodiedMethodDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more informat
+using ExpressionBodiedMethodDemo;
+
+Console.WriteLine("*** Experimenting with a Lambda Expression with an Expression-Bodied Method ***");
+Console.WriteLine();
+
+int result1 = Test.CalculateSum1(5, 7);
+Console.WriteLine($"Using a normal method, CalaculateSum1(5, 7) results: {result1}");
+Console.WriteLine();
+
+int result2 = Test.CalculateSum2(5, 7);
+Console.WriteLine($"Using expression syntax for CalaculateSum1(5, 7) results: {result2}");
+Console.WriteLine();
---
+++ ExpressionBodiedMethodDemo/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpressionBodiedMethodDemo;
+
+internal class Test
+{
+    public static int CalculateSum1(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int CalculateSum2(int a, int b) => a + b;
+}
