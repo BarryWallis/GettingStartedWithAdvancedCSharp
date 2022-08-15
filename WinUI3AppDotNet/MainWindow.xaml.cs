41c319b4153b482ae0216978ad07bc7cbe4975a1
Barry Wallis <barry.wallis@outlook.com>
2022-08-14 22:58:25 -0700

    Added WinUI3AppDotNet (updated version of WindowsFormsAppDotNetFramework

---
+++ WinUI3AppDotNet/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3AppDotNet
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private async void Button_Click(object _, RoutedEventArgs _1)
+        {
+            ContentDialog contentDialog = new()
+            {
+                Title = "",
+                Content = "Hello Reader.",
+                CloseButtonText = "OK",
+                XamlRoot = Content.XamlRoot,
+            };
+            _ = await contentDialog.ShowAsync();
+        }
+    }
+}
