bc1b0e244046c158d38d8e3a00a19bc944c76cda
Barry Wallis <barry.wallis@outlook.com>
2022-08-15 18:02:56 -0700

    Added ExpressionBodiedPropertiesDemo

---
+++ ExpressionBodiedPropertiesDemo/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ExpressionBodiedPropertiesDemo;
+
+internal class Employee
+{
+#pragma warning disable IDE0052 // Remove unread private members
+    private readonly int _employeeId;
+#pragma warning restore IDE0052 // Remove unread private members
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Company { get; } = "XYZ Ltd.";
+
+
+    public Employee(int employeeId) => _employeeId = employeeId;
+}
---
+++ ExpressionBodiedPropertiesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExpressionBodiedPropertiesDemo;
+
+Console.WriteLine("*** Experimenting with Lambda Expressions with Expression-Bodied Properties ***");
+
+Employee _employee = new(1)
+{
+    Name = "Rohan Roy"
+};
+Console.WriteLine($"{_employee.Name} works in {_employee.Company}");
