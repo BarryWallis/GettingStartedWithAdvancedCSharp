b55bd33236bc394cdb454fcbe65d29c603a2bb2c
Barry Wallis <barry.wallis@outlook.com>
2022-08-17 16:58:26 -0700

    Added NonGenericProgram2

---
+++ NonGenericProgram2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("*** Use Generics to Avoid Runtime Errors ***");
+
+ArrayList myList = new()
+{
+    1,
+    2,
+    "Invalid Element!"
+};
+foreach (int myInt in myList)
+{
+    Console.WriteLine(myInt);
+}
