7d62f5ca52b1cf90c18932816f5621034b92bf3d
Barry Wallis <barry.wallis@outlook.com>
2022-08-17 13:31:41 -0700

    Added GenericProgramDemo1

---
+++ GenericProgramDemo1/GenericClassDemo.cs
@@ -0,0 +1,8 @@
+﻿namespace GenericProgramDemo1;
+
+internal class GenericClassDemo<T>
+{
+#pragma warning disable CA1822 // Mark members as static
+    public T Display(T value) => value;
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ GenericProgramDemo1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericProgramDemo1;
+
+Console.WriteLine("*** Introduction to Generic Programming ***");
+
+GenericClassDemo<int> myGenericClassInt = new();
+Console.WriteLine($"Display(T) returns: {myGenericClassInt.Display(1)}");
+
+GenericClassDemo<string> myGenericClassString = new();
+Console.WriteLine($"Display(T) returns: {myGenericClassString.Display("A generic method was called.")}");
+
+GenericClassDemo<double> myGenericClassDouble = new();
+Console.WriteLine($"Display(T) returns: {myGenericClassDouble.Display(123.45)}");
