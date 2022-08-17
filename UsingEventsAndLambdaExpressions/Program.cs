914102f3b6f08888a0b7f8f871eb3bdffce6d21d
Barry Wallis <barry.wallis@outlook.com>
2022-08-16 18:06:59 -0700

    Added UsingEventsAndLambdaExpressions

---
+++ UsingEventsAndLambdaExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UsingEventsAndLambdaExpressions;
+
+Console.WriteLine("*** Exploring Events With Lambda Expressions ***");
+
+Sender sender = new();
+EventHandler myEvent = (sender, eventArgs)
+    => Console.WriteLine($"Using lambda expression, inside Main(), recieved a notification: Sender " +
+    $"changed MyInt");
+sender.MyIntChanged += myEvent;
+
+sender.MyInt = 1;
+sender.MyInt = 2;
+Console.WriteLine();
+
+sender.MyIntChanged -= myEvent;
+sender.MyInt = 3;
---
+++ UsingEventsAndLambdaExpressions/Sender.cs
@@ -0,0 +1,22 @@
+﻿namespace UsingEventsAndLambdaExpressions;
+
+internal class Sender
+{
+	private int _myInt;
+	public int MyInt
+	{
+		get => _myInt;
+		set
+		{
+			if (_myInt != value)
+			{
+				_myInt = value;
+				OnMyIntChanged();
+			}
+		}
+	}
+
+	public event EventHandler? MyIntChanged;
+
+	private void OnMyIntChanged() => MyIntChanged?.Invoke(this, EventArgs.Empty);
+}
