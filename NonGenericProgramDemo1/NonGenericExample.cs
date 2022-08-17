c864ae11a3546e92db0e33243ef335dad5e6f491
Barry Wallis <barry.wallis@outlook.com>
2022-08-17 13:08:07 -0700

    Chapter 4: NonGenericProgramDemo1

---
+++ NonGenericProgramDemo1/NonGenericExample.cs
@@ -0,0 +1,10 @@
+﻿namespace NonGenericProgramDemo1;
+
+internal class NonGenericExample
+{
+#pragma warning disable CA1822 // Mark members as static
+    public int DisplayMyInteger(int myInt) => myInt;
+
+    public string DisplayMyString(string myString) => myString;
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ NonGenericProgramDemo1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NonGenericProgramDemo1;
+
+Console.WriteLine("*** A Non-Generic Program Demonstration ***");
+
+NonGenericExample nonGenericExample = new();
+Console.WriteLine($"DisplayMyInteger(int): {nonGenericExample.DisplayMyInteger(123)}");
+Console.WriteLine($"DisplayMyString(string): " +
+    $"{nonGenericExample.DisplayMyString("DisplayMyString(string) inside NonGenericProgramDemo1 was called")}");
