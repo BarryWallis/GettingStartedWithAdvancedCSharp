1978e4561b147b9ae736fe6ace9c28668693b421
Barry Wallis <barry.wallis@outlook.com>
2022-08-17 17:13:42 -0700

    Added GenericProgramDemo2

---
+++ GenericProgramDemo2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Using Generics to Avoid Run-TIme Errors ***");
+
+List<int> myList = new()
+{
+    10,
+    20
+};
+foreach (int myInt in myList)
+{
+    Console.WriteLine(myInt);
+}
