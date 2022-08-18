194a7552305c2c92708298ff56dc9b546e8b6df8
Barry Wallis <barry.wallis@outlook.com>
2022-08-18 14:18:04 -0700

    Added CovarianceWithGenericDelegates

---
+++ CovarianceWithGenericDelegates/Bus.cs
@@ -0,0 +1,5 @@
+﻿namespace CovarianceWithGenericDelegates;
+
+internal class Bus : Vehicle
+{
+}
---
+++ CovarianceWithGenericDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CovarianceWithGenericDelegates;
+
+Console.WriteLine("*** Testing Ccovariance With a Generic Delegte ***");
+
+Console.WriteLine("Normal ussge:");
+CovariantDelegate<Vehicle> vehicle = GetOneVehicle;
+vehicle();
+CovariantDelegate<Bus> bus = GetOneBus;
+bus();
+Console.WriteLine();
+
+Console.WriteLine("Using covariance:");
+vehicle = bus;
+vehicle();
+Console.WriteLine();
+
+Console.WriteLine("End of covariance testing.");
+
+Bus GetOneBus()
+{
+    Console.WriteLine("Creating one bus and returning it.");
+    return new Bus();
+}
+
+Vehicle GetOneVehicle()
+{
+    Console.WriteLine("Creating one vehicle and returning it.");
+    return new Bus();
+}
+
+internal delegate TResult CovariantDelegate<out TResult>();
