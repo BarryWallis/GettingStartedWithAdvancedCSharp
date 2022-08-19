e9485f5656de9df6fc5d80643cb19a39587605ce
Barry Wallis <barry.wallis@outlook.com>
2022-08-18 20:49:35 -0700

    Added MethodOverloadingDemo

---
+++ MethodOverloadingDemo/MyClass.cs
@@ -0,0 +1,20 @@
+﻿namespace MethodOverloadingDemo;
+
+internal class MyClass<T, U>
+{
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CA1822 // Mark members as static
+    public void MyMethod(T parameter1, U parameter2)
+        => Console.WriteLine("Inside MyMethod(T parameter1, U parameter2)");
+
+    public void MyMethod(U parameter1, T parameter2)
+        => Console.WriteLine("Inside MyMethod(U parameter1, T parameter2)");
+
+    public void MyMethod2(int a, double b)
+        => Console.WriteLine("Inside MyMethod2(int a, double b)");
+
+    public void MyMethod2(double b, int a)
+        => Console.WriteLine("Inside MyMethod2(double b, int a)");
+#pragma warning restore CA1822 // Mark members as static
+#pragma warning restore IDE0060 // Remove unused parameter
+}
---
+++ MethodOverloadingDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MethodOverloadingDemo;
+
+Console.WriteLine("*** Method Overloading Demo ***");
+
+MyClass<int, double> object1 = new();
+object1.MyMethod(1, 2.3);
+object1.MyMethod2(1, 2.3);
+object1.MyMethod2(2.3, 1);
