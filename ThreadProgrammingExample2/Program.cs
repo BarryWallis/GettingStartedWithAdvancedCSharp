61bd8a2cdd95e489411b5c5b03baa32838c78807
Barry Wallis <barry.wallis@outlook.com>
2022-08-19 22:05:13 -0700

    Chapter 5: ThreadProgrammingExample1 and ThreadProgrammingExample2

---
+++ ThreadProgrammingExample2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Thread Demonstration 2***");
+
+Console.WriteLine("Main thread has started.");
+Thread thread1 = new(Method1);
+Thread thread2 = new(Method2);
+
+Console.WriteLine("Starting thread1.");
+thread1.Start();
+
+Console.WriteLine("Starting thread2.");
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+Console.WriteLine("Control comes at the end of Main().");
+
+static void Method1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"-thread1 from Method1() prints {i}");
+    }
+}
+
+static void Method2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"-thread2 from Method2() prints 2.0{i}");
+    }
+}
