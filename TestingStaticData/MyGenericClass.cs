6b0bc766e0aa1bf72a591c39503c2163522a291a
Barry Wallis <barry.wallis@outlook.com>
2022-08-19 21:25:46 -0700

    Added TestingStaticData

---
+++ TestingStaticData/MyGenericClass.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingStaticData;
+
+internal class MyGenericClass<T>
+{
+    public static int Count { get; private set; } = 0;
+
+#pragma warning disable CA1822 // Mark members as static
+    public void IncrementMe() => Console.WriteLine($"Incremented value is {++Count}");
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ TestingStaticData/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestingStaticData;
+
+Console.WriteLine("*** Testing Static in the Conext of Generic Programming ***");
+
+Console.WriteLine("Using intObject.");
+MyGenericClass<int> intObject = new();
+intObject.IncrementMe();
+intObject.IncrementMe();
+intObject.IncrementMe();
+Console.WriteLine();
+
+Console.WriteLine("Using stringObject.");
+MyGenericClass<string> stringObject = new();
+stringObject.IncrementMe();
+stringObject.IncrementMe();
+Console.WriteLine();
+
+Console.WriteLine("Using doubleObject.");
+MyGenericClass<double> doubleObject = new();
+doubleObject.IncrementMe();
+doubleObject.IncrementMe();
+Console.WriteLine();
+
+Console.WriteLine("Using intObject2.");
+MyGenericClass<int> intObject2 = new();
+intObject2.IncrementMe();
+intObject2.IncrementMe();
