ee2f0507f79a7f22b364795b280598298ad06a13
Barry Wallis <barry.wallis@outlook.com>
2022-08-22 13:59:17 -0700

    Added ThreadProgrammingExample5

---
+++ ThreadProgrammingExample5/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Threaded Methods With Return Types ***");
+
+Console.WriteLine($"Inside Main(), ManagedThreadId: {Environment.CurrentManagedThreadId}");
+int myInt = 0;
+Thread thread1 = new(() =>
+{
+    Console.WriteLine($"Method1() is executing in ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    myInt = 5;
+});
+
+string myString = "Failure";
+Thread thread2 = new(() =>
+{
+    Console.WriteLine($"Method2() is executing in ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    myString = "Success.";
+});
+
+Console.WriteLine("Starting thread1...");
+thread1.Start();
+Console.WriteLine("Starting thread2...");
+thread2.Start();
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"Method1() returns {myInt}");
+Console.WriteLine($"Method2() returns {myString}");
