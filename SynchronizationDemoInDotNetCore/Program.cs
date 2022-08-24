a2d73085927786b39a22d7a152345ec510db9fd1
Barry Wallis <barry.wallis@outlook.com>
2022-08-24 12:35:17 -0700

    Added SynchronizationDemoInDotNetCore

---
+++ SynchronizationDemoInDotNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Exploring Thread Synchronization ***");
+Console.WriteLine("*** With a Non-Syncronized Version ***");
+
+Thread.CurrentThread.Name = "Main Thread";
+Console.WriteLine("Main Thread starting...");
+
+SharedResource sharedResource = new();
+Thread thread1 = new(sharedResource.SharedMethod)
+{
+    Name = "Child Thread-1"
+};
+
+Thread thread2 = new(sharedResource.SharedMethod)
+{
+    Name = "Child THread-2"
+};
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"The {Thread.CurrentThread.Name} exiting.");
---
+++ SynchronizationDemoInDotNetCore/SharedResource.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class SharedResource
+{
+    public void SharedMethod()
+    {
+        lock (this)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} starting....");
+            Thread.Sleep(3000);
+            Console.WriteLine($"{Thread.CurrentThread.Name} exiting.");
+        }
+    }
+}
