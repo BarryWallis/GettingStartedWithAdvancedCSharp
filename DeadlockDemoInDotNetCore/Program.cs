618eaad0cb2145f7ec651235029511a1fad685f6
Barry Wallis <barry.wallis@outlook.com>
2022-08-24 13:03:28 -0700

    Added DeadlockDemoInDotNetCore

---
+++ DeadlockDemoInDotNetCore/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Exploring Deadlock With an Incorrectly Designed Application ***");
+
+Thread.CurrentThread.Name = "Main Thread";
+Console.WriteLine($"{Thread.CurrentThread.Name} starting...");
+
+SharedResource sharedResource = new();
+Thread thread1 = new(sharedResource.SharedMethod1)
+{
+    Name = "Child Thread-1",
+};
+
+Thread thread2 = new(sharedResource.SharedMethod2)
+{
+    Name = "Child Thread-2",
+};
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"...{Thread.CurrentThread.Name} exiting.");
---
+++ DeadlockDemoInDotNetCore/SharedResource.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class SharedResource
+{
+    private readonly object _lock1 = new();
+    private readonly object _lock2 = new();
+
+    public void SharedMethod1()
+    {
+        lock (_lock1)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} lock1 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"...{Thread.CurrentThread.Name} lock1 exiting.");
+            lock (_lock2)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} lock2 starting...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"...{Thread.CurrentThread.Name} lock2 exiting.");
+            }
+        }
+    }
+
+    public void SharedMethod2()
+    {
+        lock (_lock2)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} lock2 starting...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"...{Thread.CurrentThread.Name} lock2 exiting.");
+            lock (_lock1)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} lock1 starting...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"...{Thread.CurrentThread.Name} lock1 exiting.");
+            }
+        }
+    }
+}
