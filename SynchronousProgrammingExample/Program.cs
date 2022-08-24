db4db253bec2659ec179fa8be8bc8fb8dd6b5fef
Barry Wallis <barry.wallis@outlook.com>
2022-08-24 15:06:06 -0700

    Chapter 6: Added SynchronousProgrammingExample

---
+++ SynchronousProgrammingExample/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** A Synchronous Programming Example ***");
+
+
+Method1();
+Method2();
+
+Console.WriteLine("End Main()");
+
+void Method1()
+{
+    Console.WriteLine("Method1() starting...");
+    Thread.Sleep(1000);
+    Console.WriteLine("...Method1() ending.");
+}
+
+void Method2()
+{
+    Console.WriteLine("Method2() starting...");
+    Thread.Sleep(100);
+    Console.WriteLine("...Method2() ending.");
+}
