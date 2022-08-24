edd51ebb2214022fa37fc6d6a149b6121b1b2cd4
Barry Wallis <barry.wallis@outlook.com>
2022-08-24 12:25:39 -0700

    Added ExploringTheNeedForSynchronizationInDotNetCore

---
+++ ExploringTheNeedForSynchronizationInDotNetCore/SharedResource.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class SharedResource
+{
+#pragma warning disable CA1822 // Mark members as static
+    public void SharedMethod()
+#pragma warning restore CA1822 // Mark members as static
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name} starting....");
+        Thread.Sleep(3000);
+        Console.WriteLine($"{Thread.CurrentThread.Name} exiting.");
+    }
+}
