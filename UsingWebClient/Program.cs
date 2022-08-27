45908d6da8fc6eff9168f799dd0f3ef0177aba02
Barry Wallis <barry.wallis@outlook.com>
2022-08-27 16:17:10 -0700

    Added UsingWebClient

---
+++ UsingWebClient/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Net;
+
+Console.WriteLine("*** Event Based Asynchronous Program Demostration ***");
+
+bool isDownloadComplete = false;
+Console.WriteLine("Main() startng...");
+
+Console.WriteLine("Starting download...");
+#pragma warning disable SYSLIB0014 // Type or member is obsolete
+WebClient webClient = new();
+#pragma warning restore SYSLIB0014 // Type or member is obsolete
+Uri uri =
+    new(@"C:\Users\Barry\source\repos\GettingStartedWithAdvancedCSharp\UsingWebClient\"
+        + @"OriginalFile.txt");
+string targetLocation =
+    @"C:\Users\Barry\source\repos\GettingStartedWithAdvancedCSharp\"
+    + @"UsingWebClient\DownloadedFile.txt";
+webClient.DownloadFileCompleted += Completed;
+webClient.DownloadFileAsync(uri, targetLocation);
+Method2();
+
+Console.WriteLine("...Main() ending.");
+while (!isDownloadComplete)
+{
+    //
+}
+
+void Method2()
+{
+    Console.WriteLine("Method2() starting...");
+    Console.WriteLine("...Method2() ending.");
+}
+
+void Completed(object? sender, AsyncCompletedEventArgs e)
+{
+    if (e.Error is null)
+    {
+        Console.WriteLine("Download completed!");
+    }
+    else
+    {
+        Console.WriteLine(e.Error.Message);
+    }
+
+    isDownloadComplete = true;
+}
