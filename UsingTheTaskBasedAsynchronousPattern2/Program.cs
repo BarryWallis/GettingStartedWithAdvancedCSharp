c51a4bcdd6697658b51128a29e10a56113d89248
Barry Wallis <barry.wallis@outlook.com>
2022-08-28 15:26:19 -0700

    Updated UsingTheTaskBasedAsynchronousPattern2 to use ContinueWith()

--- UsingTheTaskBasedAsynchronousPattern2/Program.cs
+++ UsingTheTaskBasedAsynchronousPattern2/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*** Using the Task-Based Asynchronous Pattern with a Lambda Expression ***");
+Console.WriteLine("*** Using the Task-Based Asynchronous Pattern with a Lambda Expression (version 2) ***");
 
 Console.WriteLine($"Main() ManagedThreadId: {Environment.CurrentManagedThreadId}");
 
 Task<string> taskForMethod1 = Method1();
 
+Task taskForMethod3 = taskForMethod1.ContinueWith(method3, TaskContinuationOptions.OnlyOnRanToCompletion);
+
 Method2();
 
 Console.WriteLine($"Task for Method1() returned: {taskForMethod1.Result}");
+Task.WaitAll(taskForMethod1, taskForMethod3);
 
 static Task<string> Method1()
 {
@@ -36,4 +39,13 @@ static void Method2()
 	Console.WriteLine($"Method2(), ThreadId: {Environment.CurrentManagedThreadId} starting...");
 	Thread.Sleep(100);
 	Console.WriteLine("...Method2() ending.");
+}
+
+static void method3(Task task)
+{
+	Console.WriteLine($"Method3(), CurrentId: {Task.CurrentId}, ThreadId: " +
+		$"{Environment.CurrentManagedThreadId} starting...");
+	Thread.Sleep(20);
+	Console.WriteLine($"...Method3(), CurrentId: {Task.CurrentId}, ThreadId: " +
+		$"{Environment.CurrentManagedThreadId} ending.");
 }
