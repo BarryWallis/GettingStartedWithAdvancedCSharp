18a9759b87e1706a757ab74b38ef39c49857654b
Barry Wallis <barry.wallis@outlook.com>
2022-08-28 11:44:18 -0700

    Added UsingTheTaskBasedAsynchronousPattern

---
+++ CreatingTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Different Methods for Creating Tasks ***");
+
+Console.WriteLine($"Main().ThreadId: {Thread.CurrentThread.ManagedThreadId} starting...");
+
+Task task1 = new(MyMethod);
+task1.Start();
+
+TaskFactory taskFactory = new();
+Task task2 = taskFactory.StartNew(MyMethod);
+Task task3 = taskFactory.StartNew(MyMethod);
+
+Task.WaitAll(task1, task2, task3);
+
+Console.WriteLine($"...Main().ThreadId: {Thread.CurrentThread.ManagedThreadId} ending.");
+
+void MyMethod()
+{
+    Console.WriteLine($"Task.CurrentId: {Task.CurrentId}, Thread.CurrentThread.ManagedThreadId: " +
+        $"{Thread.CurrentThread.ManagedThreadId} starting...");
+    Thread.Sleep(100);
+    Console.WriteLine($"...Task.CurrentId: {Task.CurrentId}, Thread.CurrentThread.ManagedThreadId: " +
+        $"{Thread.CurrentThread.ManagedThreadId} ending.");
+}
+
---
+++ UsingTheTaskBasedAsynchronousPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("*** Using The Task-Based Asynchronous Pattern ***");
+
+Console.WriteLine($"Main() ManagedThreadId: " +
+    $" {Environment.CurrentManagedThreadId}");
+Task taskForMethod1 = new(Method1);
+taskForMethod1.Start();
+
+Method2();
+
+taskForMethod1.Wait();
+
+void Method1()
+{
+    Console.WriteLine("Method1() starting...");
+    Console.WriteLine($"Method1, ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    Thread.Sleep(3000);
+    Console.WriteLine("...Method1() ending.");
+}
+
+void Method2()
+{
+    Console.WriteLine("Method2() starting...");
+    Console.WriteLine($"Method2, ManagedThreadId: {Environment.CurrentManagedThreadId}");
+    Thread.Sleep(100);
+    Console.WriteLine("...Method2() ending.");
+}
